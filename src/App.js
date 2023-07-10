8e16c741cd3f0227707f997c1cd3bd651cc79146
aismeg <0>
2023-07-10 05:09:03 +0300

    ongoing

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import { Main } from "./Components/Main/Main";
 import { Feautres } from "./Components/Section/Features";
 import { Footer } from "./Components/Footer/Footer";
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Feautres />
-      <Footer />
-    </>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Main />
+        <Feautres />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
