ef039aa0e1abe78c093677e6abd1988cdbce5624
aismeg <0>
2023-07-10 15:12:37 +0300

    ongoing

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import { Header } from "./Components/Header/Header";
 import { Main } from "./Components/Main/Main";
 import { Feautres } from "./Components/Section/Features";
 import { Footer } from "./Components/Footer/Footer";
+import FetchData from "./service/Fetchdata";
 
 class App extends React.Component {
+
+  fetchData = new FetchData();
 
   state = {
     rocket: 'Falcon 1',
   };
+
+  componentDidMount() {
+    console.log(this.fetchData);
+  }
 
   render() {
     return (
